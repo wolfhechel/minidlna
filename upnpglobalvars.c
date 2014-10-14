556f90c6883421c6280f701b8c9b59d8d31e8cd6
Pontus Carlsson <pontus@thefarm.se>
2014-10-14 16:35:18 +0200

    Configurable run path

--- upnpglobalvars.c
+++ upnpglobalvars.c
@@ -60,7 +60,7 @@ time_t startup_time = 0;
 struct runtime_vars_s runtime_vars;
 uint32_t runtime_flags = INOTIFY_MASK;
 
-const char *pidfilename = "/var/run/minidlna/minidlna.pid";
+const char *pidfilename = DEFAULT_RUN_PATH "/minidlna/minidlna.pid";
 
 char uuidvalue[] = "uuid:00000000-0000-0000-0000-000000000000";
 char modelname[MODELNAME_MAX_LEN] = ROOTDEV_MODELNAME;
@@ -79,7 +79,7 @@ struct lan_addr_s lan_addr[MAX_LAN_ADDR];
 int sssdp = -1;
 
 /* Path of the Unix socket used to communicate with MiniSSDPd */
-const char * minissdpdsocketpath = "/var/run/minissdpd.sock";
+const char * minissdpdsocketpath = DEFAULT_RUN_PATH "/minissdpd.sock";
 
 /* UPnP-A/V [DLNA] */
 sqlite3 *db;
